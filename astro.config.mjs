73821361e7cea21531453326ad3ad2d87468db34
John Zanussi <johnzanussi@gmail.com>
2025-03-03 13:23:24 -0500

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [
+      tailwindcss()
+    ],
   }
 });
